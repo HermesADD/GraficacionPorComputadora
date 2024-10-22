d4f33bfa3a500ba8985c1100429a0a9829918bfa
¨HermesAlbertoDelgadoDiaz¨ <hermesdelgado@ciencias.unam.mx>
2024-10-22 02:39:11 -0600

    DodecaedroBien

--- Practica05/geometry/Dodecaedro.js
+++ Practica05/geometry/Dodecaedro.js
@@ -42,27 +42,27 @@ class Dodecaedro extends GenericGeometry{
 
   getFaces() {
     const pentagonalFaces = [
-      [0, 8, 10, 2, 16],   // Cara 1 - Ajustado
-      [1, 17, 16, 12, 0],  // Cara 2 - Ajustado (invertido)
-      [0, 12, 14, 4, 8],   // Cara 3 - Ajustado
-      [2, 16, 17, 13, 3],  // Cara 4 - Ajustado (invertido)
-      [2, 10, 6, 15, 13],  // Cara 5 - Ajustado
-      [6, 18, 4, 8, 10],   // Cara 6 - Ajustado
-      [3, 17, 11, 9, 1],   // Cara 7 - Ajustado (invertido)
-      [3, 13, 15, 7, 11],  // Cara 8 - Ajustado
-      [1, 9, 5, 14, 12],   // Cara 9 - Ajustado
-      [5, 19, 7, 11, 9],   // Cara 10 - Ajustado
-      [5, 14, 4, 18, 19],  // Cara 11 - Ajustado
-      [6, 18, 19, 7, 15]   // Cara 12 - Ajustado
-    ];
+      [0, 16, 2, 10, 8],   // Cara 1 - Ajustado (sentido antihorario)
+      [0, 12, 16, 17, 1],  // Cara 2 - Ajustado (sentido antihorario)
+      [0, 8, 4, 14, 12],   // Cara 3 - Ajustado (sentido antihorario)
+      [2, 13, 17, 16, 3],  // Cara 4 - Ajustado (sentido antihorario)
+      [2, 10, 6, 15, 13],  // Cara 5 - Ajustado (sentido antihorario)
+      [6, 18, 4, 8, 10],   // Cara 6 - Ajustado (sentido antihorario)
+      [3, 17, 11, 9, 1],   // Cara 7 - Ajustado (sentido antihorario)
+      [3, 13, 15, 7, 11],  // Cara 8 - Ajustado (sentido antihorario)
+      [1, 12, 14, 5, 9],   // Cara 9 - Ajustado (sentido antihorario)
+      [5, 19, 7, 11, 9],   // Cara 10 - Ajustado (sentido antihorario)
+      [5, 14, 4, 18, 19],  // Cara 11 - Ajustado (sentido antihorario)
+      [6, 15, 7, 19, 18]   // Cara 12 - Ajustado (sentido antihorario)
+  ];
     let triangles = [];
 
     // Dividir cada pentágono en 3 triángulos con el orden correcto de vértices
     for (let i = 0; i < pentagonalFaces.length; i++) {
         let face = pentagonalFaces[i];
-        triangles.push(face[0], face[1], face[2]);
-        triangles.push(face[0], face[2], face[3]);
-        triangles.push(face[0], face[3], face[4]);
+        triangles.push(face[0], face[2], face[1]);
+        triangles.push(face[0], face[3], face[2]);
+        triangles.push(face[0], face[4], face[3]);
     }
 
     return triangles;
